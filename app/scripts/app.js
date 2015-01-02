b8a4ff2874c6b5e8621aa3a489e654f5e2f20332
Shidhin C R <shidhincr@gmail.com>
2015-01-02 09:27:49 +0400

    Implementing the react router and basic home and search page views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,45 +1,37 @@
 /** @jsx React.DOM */
 
-var React = window.React = require('react'),
-    Timer = require("./ui/Timer"),
-    mountNode = document.getElementById("app");
+var React = window.React = require('react');
+var Timer = require("./ui/Timer");
+var mountNode = document.getElementById("app");
+var Router = require('react-router');
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+var RouteHandler = Router.RouteHandler;
 
-var TodoList = React.createClass({
-  render: function() {
-    var createItem = function(itemText) {
-      return <li>{itemText}</li>;
-    };
-    return <ul>{this.props.items.map(createItem)}</ul>;
-  }
-});
-var TodoApp = React.createClass({
-  getInitialState: function() {
-    return {items: [], text: ''};
-  },
-  onChange: function(e) {
-    this.setState({text: e.target.value});
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var nextItems = this.state.items.concat([this.state.text]);
-    var nextText = '';
-    this.setState({items: nextItems, text: nextText});
-  },
-  render: function() {
+// views
+var Home = require('./views/home');
+var Search = require('./views/search');
+
+var App = React.createClass({
+  render: function(){
     return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
-        <Timer />
+      <div className="container">
+        <RouteHandler/>
       </div>
     );
   }
 });
 
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="search" handler={Search}/>
+    <DefaultRoute handler={Home}/>
+    <NotFoundRoute handler={Home} />
+  </Route>
+);
 
-React.renderComponent(<TodoApp />, mountNode);
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, mountNode);
+});
 
