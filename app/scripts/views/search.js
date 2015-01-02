71407482f15efe454dc59a10bcc00d418284e5f3
Shidhin C R <shidhincr@gmail.com>
2015-01-02 09:27:49 +0400

    Implementing the react router and basic home and search page views

---
+++ app/scripts/views/search.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Search  = React.createClass({
+    render: function(){
+        return (
+            <div className="search">
+                Have to implement the search view here 
+            </div>
+        )
+    }
+});
+
+module.exports = Search;
