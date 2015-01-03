dac6e2be82ef614e90b32262f6df6b154e14d760
Shidhin C R <shidhincr@gmail.com>
2015-01-03 08:31:24 +0400

    Search page layout using reactjs bootstrap

---
+++ app/scripts/ui/filters.js
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var Filters = React.createClass({
+    displayName: 'Filters',
+    render: function () {
+        return (
+            <div>Filters</div>
+        );
+    }
+});
+
+module.exports = Filters;
---
+++ app/scripts/ui/navbar.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+/** @jsx React.DOM */
+var Navbar = React.createClass({
+    displayName: 'Navbar',
+    render: function () {
+        return (
+            <div>Navbar</div>
+        );
+    }
+});
+
+module.exports = Navbar
