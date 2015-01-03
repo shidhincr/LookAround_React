144918be08eacee85859af33b9a75c269a514c1b
Shidhin C R <shidhincr@gmail.com>
2015-01-03 08:31:24 +0400

    Search page layout using reactjs bootstrap

---
+++ app/scripts/ui/results.js
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var Results = React.createClass({
+    displayName: 'Results',
+    render: function () {
+        return (
+            <div>Results</div>
+        );
+    }
+});
+
+module.exports = Results;
--- app/scripts/views/search.js
+++ app/scripts/views/search.js
@@ -1,9 +1,17 @@
 var React = require('react');
+var Navbar = require('../ui/navbar.js');
+var Filters = require('../ui/filters.js');
+var Results = require('../ui/results.js');
+
 var Search  = React.createClass({
     render: function(){
         return (
             <div className="search">
-                Have to implement the search view here 
+                <Navbar />
+                <div className="row-fluid">
+                    <Filters />
+                    <Results />
+                </div>
             </div>
         )
     }
