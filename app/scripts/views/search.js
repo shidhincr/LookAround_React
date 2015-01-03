cebd626a6417e71067374324b91efc8ee21e3d86
Shidhin C R <shidhincr@gmail.com>
2015-01-03 09:26:45 +0400

    Navbar using react bootstrap

--- app/scripts/views/search.js
+++ app/scripts/views/search.js
@@ -1,19 +1,29 @@
 var React = require('react');
-var Navbar = require('../ui/navbar.js');
+var Header = require('../ui/header.js');
 var Filters = require('../ui/filters.js');
 var Results = require('../ui/results.js');
+var Grid = require('react-bootstrap/Grid');
+var Row = require('react-bootstrap/Row');
+var Col = require('react-bootstrap/Col');
+
 
 var Search  = React.createClass({
     render: function(){
         return (
-            <div className="search">
-                <Navbar />
-                <div className="row-fluid">
-                    <Filters />
-                    <Results />
-                </div>
-            </div>
-        )
+            <Grid fluid>
+                <Row>
+                    <Header />
+                </Row>
+                <Row>
+                    <Col xs={3}>
+                        <Filters />
+                    </Col>
+                    <Col xs={9}>
+                        <Results />
+                    </Col>
+                </Row>
+            </Grid>
+        );
     }
 });
 
