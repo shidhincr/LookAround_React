ac5c544a69cb9d87e9058d1098be23e31d09fe2a
ivinjose <ivinjose@flipkarts-MacBook-Pro-4.local>
2015-01-30 05:23:26 +0530

    Implementing zipcode validation #20

--- app/scripts/ui/ZipcodeInput.js
+++ app/scripts/ui/ZipcodeInput.js
@@ -6,16 +6,33 @@ var Button  = require('react-bootstrap/Button');
 
 var ZipcodeInput = React.createClass({
     displayName: 'ZipcodeInput',
+    validate: function(e){
+        var zipcodeRegex = /^\d{6}$/;
+        var zipcode = e.target.value;
+        if( zipcode.length ){
+            if( zipcodeRegex.test(zipcode) ){
+                $('.goButton').removeAttr('disabled');
+                $('.zipcodeValidCheck').show();
+            }else{
+                $('.goButton').attr('disabled','disabled');
+                $('.zipcodeValidCheck').hide();
+            }
+        }else{
+            $('.zipcodeValidCheck').hide();
+        }
+    },
     render: function(){
-        var zipcodeComponentStyle = {width: '300px'}; 
+        var zipcodeComponentStyle = {width: '300px'};
         return(
             <div className="zipcodeComponent" style={zipcodeComponentStyle}>
                 <Input
                     type="text"
                     placeholder="560068"
                     className="zipcodeInput"
-                    buttonAfter={<Button bsStyle="primary">Go</Button>}
+                    buttonAfter={<Button bsStyle="primary" className="goButton" disabled>Go</Button>}
+                    onChange={this.validate}
                 />
+                <span className="zipcodeValidCheck">Valid zipcode</span>
             </div>
         );
     }
