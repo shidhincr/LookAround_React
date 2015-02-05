7ec3f7d160c7c355e205c4b23b4259ffb232ac23
ivinjose <ivinjose@flipkarts-MacBook-Pro-5.local>
2015-02-05 18:21:54 +0530

    Restructuring map.js to easily add markers. #24

--- app/scripts/ui/Map.js
+++ app/scripts/ui/Map.js
@@ -4,27 +4,28 @@ var React  = require('react');
 var Router = require('react-router');
 
 var Map = React.createClass({
-    _getMapCenter: function(){
-        var location,
-            latlng,
-            mapSettings,
-            map,
-            _this = this,
-            geocoder = new google.maps.Geocoder();
-
-        geocoder.geocode( {'address': _this.props.zipcode}, function( results, status ){
+    _getGeocode: function(zipcode, callback){
+        var location, geocoder = new google.maps.Geocoder();
+        geocoder.geocode( {'address': zipcode}, function( results, status ){
             location = results[ 0 ].geometry.location;
-            latlng = new google.maps.LatLng( location.k, location.D );
-            mapSettings = {
-                center: latlng,
-                zoom: 15
-            };
-            map = new google.maps.Map( _this.refs.mapBackground.getDOMNode(), mapSettings );
-            map.panTo( latlng );
+            callback( location );
         });
     },
+    _setMapCenter: function( location ){
+        var latlng = new google.maps.LatLng( location.k, location.D );
+        var map = new google.maps.Map( this.refs.mapBackground.getDOMNode(), {zoom: 15} );
+        map.panTo( latlng );
+    },
+    componentDidMount: function(){
+        this._getGeocode(this.props.zipcode, this._setMapCenter);
+    },
+    componentWillReceiveProps: function(nextProps){
+        this._getGeocode(nextProps.zipcode, this._setMapCenter);
+    },
+    shouldComponentUpdate: function(nextProps, nextState){
+        return nextProps.zipcode!=this.props.zipcode;
+    },
     render: function(){
-        this._getMapCenter();
         return (
             <div className='map-background' ref='mapBackground'></div>
         );
