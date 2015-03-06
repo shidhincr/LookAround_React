66ec34e1b24e4d49a313a797a5e0cd63d5a4dc66
Shidhin C R <shidhincr@gmail.com>
2015-03-06 09:29:57 +0400

    Adding tests

--- gulpfile.js
+++ gulpfile.js
@@ -12,150 +12,146 @@ var $ = require('gulp-load-plugins')();
 var browserify = require('browserify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream'),
-    
-    sourceFile = './app/scripts/app.js',
-    
-    destFolder = './dist/scripts',
-    destFileName = 'app.js';
+
+  sourceFile = './app/scripts/app.js',
+
+  destFolder = './dist/scripts',
+  destFileName = 'app.js';
 
 
 // Styles
 gulp.task('styles', function () {
-    return gulp.src('app/styles/main.scss')
-        .pipe($.rubySass({
-            style: 'expanded',
-            precision: 10,
-            loadPath: ['app/bower_components']
-        }))
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('dist/styles'))
-        .pipe($.size());
+  return gulp.src('app/styles/main.scss')
+    .pipe($.rubySass({
+      style: 'expanded',
+      precision: 10,
+      loadPath: ['app/bower_components']
+    }))
+    .pipe($.autoprefixer('last 1 version'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe($.size());
 });
 
 
 // Scripts
 gulp.task('scripts', function () {
-    var bundler = watchify(browserify({
-        entries: [sourceFile],
-        insertGlobals: true,
-        cache: {},
-        packageCache: {},
-        fullPaths: true
-    }));
+  var bundler = watchify(browserify({
+    entries: [sourceFile],
+    insertGlobals: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  }));
 
-    bundler.on('update', rebundle);
+  bundler.on('update', rebundle);
 
-    function rebundle() {
-        return bundler.bundle()
-            // log errors if they happen
-            .on('error', $.util.log.bind($.util, 'Browserify Error'))
-            .pipe(source(destFileName))
-            .pipe(gulp.dest(destFolder));
-    }
+  function rebundle() {
+    return bundler.bundle()
+      // log errors if they happen
+      .on('error', $.util.log.bind($.util, 'Browserify Error'))
+      .pipe(source(destFileName))
+      .pipe(gulp.dest(destFolder));
+  }
 
-    return rebundle();
+  return rebundle();
 
 });
 
 // static resources
 gulp.task('static', function () {
-    return gulp.src('app/data/**/*')
-        .pipe(gulp.dest('dist/data'))
-        .pipe($.size());
+  return gulp.src('app/data/**/*')
+    .pipe(gulp.dest('dist/data'))
+    .pipe($.size());
 });
 
 // HTML
 gulp.task('html', function () {
-    return gulp.src('app/*.html')
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'))
-        .pipe($.size());
+  return gulp.src('app/*.html')
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'))
+    .pipe($.size());
 });
 
 // Images
 gulp.task('images', function () {
-    return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'))
-        .pipe($.size());
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size());
 });
-
 
 
 gulp.task('jest', function () {
-    var nodeModules = path.resolve('./node_modules');
-    return gulp.src('app/scripts/**/__tests__')
-        .pipe($.jest({
-            scriptPreprocessor: nodeModules + '/gulp-jest/preprocessor.js',
-            unmockedModulePathPatterns: [nodeModules + '/react']
-        }));
+  var nodeModules = path.resolve('./node_modules');
+  return gulp.src('app/scripts/**/__tests__')
+    .pipe($.jest({
+      scriptPreprocessor: nodeModules + '/gulp-jest/preprocessor.js',
+      unmockedModulePathPatterns: [nodeModules + '/react']
+    }));
 });
-
 
 
 // Clean
 gulp.task('clean', function (cb) {
-    del(['dist/styles', 'dist/scripts', 'dist/images'], cb);
+  del(['dist/styles', 'dist/scripts', 'dist/images'], cb);
 });
 
 
 // Bundle
-gulp.task('bundle', ['styles', 'scripts', 'static', 'bower'], function(){
-    return gulp.src('./app/*.html')
-               .pipe($.useref.assets())
-               .pipe($.useref.restore())
-               .pipe($.useref())
-               .pipe(gulp.dest('dist'));
+gulp.task('bundle', ['styles', 'scripts', 'static', 'bower'], function () {
+  return gulp.src('./app/*.html')
+    .pipe($.useref.assets())
+    .pipe($.useref.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('dist'));
 });
 
 // Build
 gulp.task('build', ['html', 'bundle', 'images']);
 
 // Default task
-gulp.task('default', ['clean', 'build', 'jest' ]);
+gulp.task('default', ['clean', 'build', 'jest']);
 
 // Webserver
 gulp.task('serve', function () {
-    gulp.src('./dist')
-        .pipe($.webserver({
-            livereload: true,
-            port: 9000
-        }));
+  gulp.src('./dist')
+    .pipe($.webserver({
+      livereload: true,
+      port: 9000
+    }));
 });
 
 // Bower helper
-gulp.task('bower', function() {
-    gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
-        .pipe(gulp.dest('dist/bower_components/'));
+gulp.task('bower', function () {
+  gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
+    .pipe(gulp.dest('dist/bower_components/'));
 
 });
 
-gulp.task('json', function() {
-    gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
-        .pipe(gulp.dest('dist/scripts/'));
+gulp.task('json', function () {
+  gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
+    .pipe(gulp.dest('dist/scripts/'));
 });
 
 
 // Watch
 gulp.task('watch', ['default', 'serve'], function () {
 
-    // Watch .json files
-    gulp.watch('app/scripts/**/*.json', ['json']);
+  // Watch .json files
+  gulp.watch('app/scripts/**/*.json', ['json']);
 
-    // Watch .html files
-    gulp.watch('app/*.html', ['html']);
-
-    
-    // Watch .scss files
-    gulp.watch('app/styles/**/*.scss', ['styles']);
-    
+  // Watch .html files
+  gulp.watch('app/*.html', ['html']);
 
 
+  // Watch .scss files
+  gulp.watch('app/styles/**/*.scss', ['styles']);
 
-    // Watch image files
-    gulp.watch('app/images/**/*', ['images']);
+
+  // Watch image files
+  gulp.watch('app/images/**/*', ['images']);
 });
